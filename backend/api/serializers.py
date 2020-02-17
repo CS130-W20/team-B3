bca17afa33b74f85708715fd64c6af65ed72cabe
Eric Dang <edang830@gmail.com>
2020-02-17 12:43:21 -0800

    Implement create and update methods

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -6,3 +6,13 @@ class LocationSerializer(serializers.Serializer):
     id = serializers.CharField(read_only=True)
     lat = serializers.DecimalField()
     lng = serializers.DecimalField()
+
+    def create(self, validated_data):
+        return models.Location.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.id = validated_data.get('id', instance.id)
+        instance.lat = validated_data.get('lat', instance.lat)
+        instance.lng = validated_data.get('lng', instance.lng)
+
+
