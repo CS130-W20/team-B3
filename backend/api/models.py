c4d9265075892cd2efc218f121b99bf68fe12826
jonathanaschultz <jonathanaschultz1@gmail.com>
2020-02-17 13:47:50 -0800

    Models: more fixes, Mongo is picky with required fields

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Location(models.Model):
-	id = models.CharField(primary_key=True)
-	lat = models.DecimalField()
-	lng = models.DecimalField()
+	id = models.CharField(max_length=255, primary_key=True)
+	lat = models.DecimalField(max_digits=9, decimal_places=6)
+	lng = models.DecimalField(max_digits=9, decimal_places=6)
 
 class DiningHall(Location):
 	open_at = models.TimeField()
 	close_at = models.TimeField()
 	name = models.CharField(max_length=50)
-	description = models.CharField(max_length=200)
+	description = models.CharField(max_length=255)
 	picture = models.URLField()
 
 class User(models.Model):
@@ -20,13 +20,13 @@ class User(models.Model):
 		('2', 'Banned')
 	]
 	status = models.CharField(max_length=1, choices=USER_STATES)
-	id = models.CharField(primary_key=True)
+	id = models.CharField(max_length=255, primary_key=True)
 	pp_email = models.EmailField()
 
 class Account(User):
 	home_loc = models.OneToOneField(Location, on_delete=models.DO_NOTHING)
-	pw = models.CharField()
-	phone = models.CharField() #Doesn't make sense to use numerical, we'll need to validate though
+	pw = models.CharField(max_length=255)
+	phone = models.CharField(max_length=30) #Doesn't make sense to use numerical, we'll need to validate though
 
 class Swipe(models.Model):
 	SWIPE_STATES = [
@@ -38,7 +38,7 @@ class Swipe(models.Model):
 	status = models.CharField(max_length=1, choices=SWIPE_STATES)
 	seller = models.OneToOneField(User, on_delete=models.DO_NOTHING)
 	location = models.ForeignKey(DiningHall, on_delete=models.DO_NOTHING) #possibly add on_delete=models.CASCADE, but we might want to keep data around
-	price = models.DecimalField(decimal_places=2)
+	price = models.DecimalField(max_digits=5, decimal_places=2)
 
 class Bid(models.Model):
 	BID_STATES = [
@@ -49,14 +49,14 @@ class Bid(models.Model):
 	status = models.CharField(max_length=1, choices=BID_STATES)
 	swipe = models.ForeignKey(Swipe, on_delete=models.CASCADE)
 	buyer = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-	bid_price = models.DecimalField(decimal_places=2)
+	bid_price = models.DecimalField(max_digits=5, decimal_places=2)
 
 class Transaction(models.Model):
-	sender = models.CharField()
-	recipient = models.CharField()
+	sender = models.CharField(max_length=255)
+	recipient = models.CharField(max_length=255)
 	paid = models.DateTimeField()
-	total = models.DecimalField(decimal_places=2)
-	details = models.CharField()
+	total = models.DecimalField(max_digits=5, decimal_places=2)
+	details = models.CharField(max_length=255)
 
 class Listing(models.Model):
 	swipe = models.ForeignKey(Swipe, on_delete=models.CASCADE)
