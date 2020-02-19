c7869d2783b6eee5649464fc5596712d9936cfd6
jonathanaschultz <jonathanaschultz1@gmail.com>
2020-02-18 17:10:32 -0800

    Serializers: Completely fixed, AccountCreate: first implementation

--- backend/api/accountviews.py
+++ backend/api/accountviews.py
@@ -7,8 +7,20 @@ from api.serializers import LocationSerializer, AccountSerializer, UserSerialize
 
 @api_view(['POST'])
 def account_create(request):
-	serializer = AccountSerializer(data=request.data)
-	if serializer.is_valid():
-		serializer.save()
-		return Response(serializer.data, status=status.HTTP_201_CREATED)
-	return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+	data = request.data
+	loc_data = data.pop('loc')
+	if type(loc_data) == dict: #If we've got a dict, that means the Location object should be created from the lat/lng
+		loc_serializer = LocationSerializer(data=loc_data)
+		if loc_serializer.is_valid():
+			loc_obj = loc_serializer.save()
+			data['home_loc'] = loc_obj.loc_id
+		else:
+			return Response(loc_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+	else: #For testing purposes, don't create a Location object but just toss in an existing primary key
+		data['home_loc'] = loc_data
+	acc_serializer = AccountSerializer(data=data)
+	if acc_serializer.is_valid():
+		acc_serializer.save()
+		return Response({'STATUS': '0'}, status=status.HTTP_200_OK)
+	else:
+		return Response(acc_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
 import api.models as api
-from bson import ObjectId
-from bson.errors import InvalidId
 
 
 class LocationSerializer(serializers.ModelSerializer):
@@ -23,79 +21,21 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class AccountSerializer(UserSerializer):
-	home_loc = LocationSerializer()
-
 	class Meta(UserSerializer.Meta):
 		model = api.Account
 		fields = UserSerializer.Meta.fields + ['home_loc', 'pw', 'phone']
 
 
-	def create(self, validated_data): #When we have nested serializers like this, we have to override create behavior :|
-		loc_data = validated_data.pop('home_loc')
-		if type(loc_data) == dict:
-			loc_obj = api.Location.objects.create(**loc_data)
-		elif type(loc_data) == api.models.Location:
-			loc_obj = loc_data
-		account = api.Account.objects.create(home_loc=loc_obj, **validated_data)
-		return account
-
-	def update(self, instance, validated_data): #As well as update behavior
-		loc_data = validated_data.pop('home_loc')
-		assert type(loc_data) == dict
-		loc_obj = instance.home_loc
-		loc_obj.lat = loc_data.get('lat', loc_obj.lat)
-		loc_obj.lng = loc_data.get('lng', loc_obj.lng)
-		loc_obj.save()
-		instance.status = validated_data.get('status', instance.status)
-		instance.pp_email = validated_data.get('pp_email', instance.pp_email)
-		instance.pw = validated_data.get('pw', instance.pw)
-		return instance
-
 class SwipeSerializer(serializers.ModelSerializer):
-	seller = UserSerializer()
-	location = DiningHallSerializer()
-
 	class Meta:
 		model = api.Swipe
 		fields = ['status', 'seller', 'location', 'price']
 
-	def create(self, validated_data): #To create a swipe, we need to ensure that the seller and dining hall are both valid
-		loc_data = validated_data.pop('hall')
-		if type(loc_data) == str:
-			loc_obj = api.DiningHall.objects.get(hall_id=loc_data)
-		elif type(loc_data) == api.models.DiningHall:
-			loc_obj = loc_data
-		seller_data = validated_data.pop('seller')
-		if type(seller_data) == str:
-			seller_obj = api.User.objects.get(user_id=seller_data)
-		elif type(seller_data) == api.models.User:
-			seller_obj = seller_data
-		swipe = api.Swipe.objects.create(location=loc_obj, seller=seller_obj, **validated_data)
-		return swipe
-
-
 
 class BidSerializer(serializers.ModelSerializer):
-	swipe = SwipeSerializer()
-	buyer = UserSerializer()
-
 	class Meta:
 		model = api.Bid
 		fields = ['status', 'swipe', 'buyer', 'bid_price']
-
-	def create(self, validated_data): #To create a bid, we need to ensure that the swipe and buyer are both valid
-		swipe_data = validated_data.pop('swipe')
-		if type(swipe_data) == str:
-			swipe_obj = api.Swipe.objects.get(swipe_id=swipe_data)
-		elif type(swipe_data) == api.models.Swipe:
-			swipe_obj = swipe_data
-		buyer_data = validated_data.pop('buyer')
-		if type(buyer_data) == str:
-			buyer_obj = api.User.objects.get(user_id=buyer_data)
-		elif type(buyer_data) == api.models.User:
-			buyer_obj = buyer_data
-		bid = api.Bid.objects.create(swipe=swipe_obj, buyer=buyer_obj, **validated_data)
-		return bid
 
 
 class TransactionSerializer(serializers.ModelSerializer):
@@ -105,23 +45,6 @@ class TransactionSerializer(serializers.ModelSerializer):
 
 
 class ListingSerializer(serializers.ModelSerializer):
-	swipe = SwipeSerializer()
-	seller_loc = LocationSerializer()
-
 	class Meta:
 		model = api.Listing
 		fields = ['swipe', 'seller_loc', 'description', 'visible_from', 'visible_to']
-
-	def create(self, validated_data): #We need to ensure that the swipe and the seller's location (if included) is valid
-		swipe_data = validated_data.pop('swipe')
-		if type(swipe_data) == str:
-			swipe_obj = api.Swipe.objects.get(swipe_id=swipe_data)
-		elif type(swipe_data) == api.models.Swipe:
-			swipe_obj = swipe_data
-		seller_loc_data = validated_data.pop('seller_loc')
-		if type(seller_loc_data) == str:
-			seller_loc_obj = api.Listing.objects.get(listing_id=seller_loc_data)
-		elif type(seller_loc_data) == api.models.Location:
-			seller_loc_obj = seller_loc_data
-		listing = api.Listing.objects.create(swipe=swipe_obj, seller_loc=seller_loc_obj, **validated_data)
-		return listing
