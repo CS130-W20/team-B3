30542eb0921369dc9315a05735da7269a6ce5748
Eric Dang <edang830@gmail.com>
2020-02-20 15:47:11 -0800

    Fix indentation error

--- backend/api/buyingviews.py
+++ backend/api/buyingviews.py
@@ -5,22 +5,23 @@ from rest_framework.response import Response
 from api.models import DiningHall, Account, User, Swipe, Bid
 from api.serializers import BidSerializer
 
+
 @api_view(['POST'])
 def place_bid(request):
     data = request.data
-    
+
     if 'user_id' not in data:
         return Response({'STATUS': '1', 'REASON': 'MISSING REQUIRED USER_ID ARGUMENT'}, status=status.HTTP_400_BAD_REQUEST)
     if 'bid_price' not in data:
         return Response({'STATUS': '1', 'REASON': 'MISSING REQUIRED BID_PRICE ARGUMENT FOR BID'}, status=status.HTTP_400_BAD_REQUEST)
-    
+
     bid_data = {'status': 0, 'buyer': data['user_id'], 'bid_price': data["bid_price"]}
     bid_serializer = BidSerializer(data=bid_data)
-    
+
     if bid_serializer.is_valid():
-            bid_obj = bid_serializer.save()
-            bid_id = bid_obj.bid_id
-            make_pair(bid_id)
-            return Response({'STATUS': '0'}, status=status.HTTP_200_OK)
-        else:
-            return Response(bid_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        bid_obj = bid_serializer.save()
+        bid_id = bid_obj.bid_id
+        make_pair(bid_id)
+        return Response({'STATUS': '0'}, status=status.HTTP_200_OK)
+    else:
+        return Response(bid_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
