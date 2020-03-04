ce799c306b67183924dfca13e72231a2de69994d
ashwin9798 <ashwin9798@gmail.com>
2020-03-03 22:01:48 -0800

    create XCode project

---
+++ frontend/SwipeX/SwipeX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeX
+//
+//  Created by Ashwin Vivek on 3/3/20.
+//  Copyright © 2020 CS 130. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
