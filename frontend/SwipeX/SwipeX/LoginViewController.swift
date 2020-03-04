238bba2a1ac64960babd069b1b2e8fcaf0326038
ashwin9798 <ashwin9798@gmail.com>
2020-03-04 12:32:06 -0800

    store login information in user defaults

--- frontend/SwipeX/SwipeX/LoginViewController.swift
+++ frontend/SwipeX/SwipeX/LoginViewController.swift
@@ -41,6 +41,19 @@ class LoginViewController: UIViewController, GIDSignInDelegate {
            return
         }
         
+        // Perform any operations on signed in user here.
+        let userId = user.userID                  // For client-side use only!
+        let idToken = user.authentication.idToken // Safe to send to the server
+        let fullName = user.profile.name
+        let givenName = user.profile.givenName
+        let familyName = user.profile.familyName
+        let email = user.profile.email
+
+        UserDefaults.standard.set(userId, forKey: "userId")
+        UserDefaults.standard.set(givenName, forKey: "givenName")
+        UserDefaults.standard.set(familyName, forKey: "familyName")
+        UserDefaults.standard.set(email, forKey: "email")
+        
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let tabbarVC = storyboard.instantiateViewController(withIdentifier: "MainTabBarVC")
             tabbarVC.modalPresentationStyle = .fullScreen
