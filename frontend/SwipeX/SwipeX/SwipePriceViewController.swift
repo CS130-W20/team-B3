621681dc4a5edcf8b2179bbbc45a3ac7be4a1200
ashwin9798 <ashwin9798@gmail.com>
2020-03-04 11:58:47 -0800

    add time picker in swipe price vc

--- frontend/SwipeX/SwipeX/SwipePriceViewController.swift
+++ frontend/SwipeX/SwipeX/SwipePriceViewController.swift
@@ -22,7 +22,19 @@ class SwipePriceViewController: UIViewController{
     
     @IBOutlet weak var lowestAskView: UIView!
     @IBOutlet weak var highestBidView: UIView!
-
+    
+    var didTapFrom: Bool?
+    var didTapBuy:Bool?
+    
+    @IBAction func didTapFrom(_ sender: Any) {
+          didTapFrom = true
+    }
+    @IBAction func didTapTo(_ sender: Any) {
+          didTapFrom = false
+    }
+    fileprivate let datePickerFrom = ToolbarDatePicker()
+    fileprivate let datePickerTo = ToolbarDatePicker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         diningHallLabel.text = diningHallName
@@ -48,8 +60,26 @@ class SwipePriceViewController: UIViewController{
         timeFormatter.timeStyle = DateFormatter.Style.short
         self.title = diningHallName
         
+        self.fromTime.inputView = self.datePickerFrom
+        self.fromTime.inputAccessoryView = self.datePickerFrom.toolbar
+        self.datePickerFrom.datePickerMode = UIDatePicker.Mode.time
+        self.datePickerFrom.minuteInterval = 30
+        self.datePickerFrom.toolbarDelegate = self
+        self.datePickerFrom.minimumDate =  timeFormatter.date(from: "5:00 pm")
+        self.datePickerFrom.maximumDate =  timeFormatter.date(from: "7:30 pm")
+        self.datePickerFrom.date = timeFormatter.date(from: "5:00 pm")!
+        
         fromTime.text = timeFormatter.string(from: timeFormatter.date(from: "5:00 pm")!)
 
+        self.toTime.inputView = self.datePickerTo
+        self.toTime.inputAccessoryView = self.datePickerFrom.toolbar
+        self.datePickerTo.datePickerMode = UIDatePicker.Mode.time
+        self.datePickerTo.minuteInterval = 30
+        self.datePickerTo.toolbarDelegate = self
+        self.datePickerTo.minimumDate =  timeFormatter.date(from: "5:30 pm")
+        self.datePickerTo.maximumDate =  timeFormatter.date(from: "8:00 pm")
+        self.datePickerTo.date = timeFormatter.date(from: "8:00 pm")!
+        
         toTime.text = timeFormatter.string(from: timeFormatter.date(from: "8:00 pm")!)
     }
 
@@ -64,4 +94,33 @@ class SwipePriceViewController: UIViewController{
     }
     */
 
+}
+
+extension SwipePriceViewController: ToolbarDatePickerDelegate {
+
+    func didTapDone() {
+        var timeFormatter = DateFormatter()
+        timeFormatter.dateStyle = DateFormatter.Style.none
+        timeFormatter.timeStyle = DateFormatter.Style.short
+        
+        if (self.didTapFrom!) {
+            fromTime.text = timeFormatter.string(from: self.datePickerFrom.date)
+            self.datePickerTo.minimumDate = self.datePickerFrom.date.addingTimeInterval(30.0)
+            self.fromTime.resignFirstResponder()
+            
+        } else {
+            toTime.text = timeFormatter.string(from: self.datePickerTo.date)
+            self.datePickerFrom.maximumDate = self.datePickerTo.date.addingTimeInterval(-30.0)
+            self.toTime.resignFirstResponder()
+        }
+        // TODO: Filter the best ask and bid
+    }
+    
+    func didTapCancel() {
+        if(self.didTapFrom!) {
+            self.fromTime.resignFirstResponder()
+        } else {
+            self.toTime.resignFirstResponder()
+        }
+    }
 }
