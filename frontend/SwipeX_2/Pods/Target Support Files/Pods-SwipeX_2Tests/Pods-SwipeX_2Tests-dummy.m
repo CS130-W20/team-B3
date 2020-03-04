45f1be56b6eb0e4ef4d17f1e36155fdc658a1a2b
ashwin9798 <ashwin9798@gmail.com>
2020-03-03 21:51:48 -0800

    delete frontend folder to restart with new commit history

--- frontend/SwipeX_2/Pods/Target Support Files/Pods-SwipeX_2Tests/Pods-SwipeX_2Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwipeX_2Tests : NSObject
-@end
-@implementation PodsDummy_Pods_SwipeX_2Tests
-@end
--- frontend/SwipeX_2/Pods/Target Support Files/Pods-SwipeX_2UITests/Pods-SwipeX_2UITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwipeX_2UITests : NSObject
-@end
-@implementation PodsDummy_Pods_SwipeX_2UITests
-@end
