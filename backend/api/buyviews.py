0b6e0c9a1b1d7aa2f15012885ecb342c1a7ea9c9
Eric Dang <edang830@gmail.com>
2020-03-04 18:31:11 -0800

    Implement buyer purchasing swipe

--- backend/api/buyviews.py
+++ backend/api/buyviews.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from api.models import DiningHall, Swipe, Bid
+from api.models import DiningHall, Swipe, Bid, User
 from api.serializers import BidSerializer, SwipeSerializer
 import datetime
+from keys import stripe_test_key, twilio_account_sid, twilio_auth_token
+from twilio.rest import Client
 
 
 def bid_getcheapestswipe(hall_id, swipe_time=None, swipe_price=None):
@@ -84,3 +86,34 @@ def bid_placebid(request):
         return Response({'STATUS': '0', 'REASON': 'BID CREATED, NO ELIGIBLE SWIPE PAIRED'}, status=status.HTTP_200_OK)
     else:
         return Response(bid_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def buy_listed_swipe(request):
+    data = request.data
+    if 'swipe_id' not in data:
+        return Response({'STATUS': '1', 'REASON': 'MISSING REQUIRED SWIPE_ID ARGUMENT'}, status=status.HTTP_400_BAD_REQUEST)
+    elif 'user_id' not in data:
+        return Response({'STATUS': '1', 'REASON': 'MISSING REQUIRED USER_ID ARGUMENT'}, status=status.HTTP_400_BAD_REQUEST)
+    try:
+        swipe = Swipe.objects.get(swipe_id=data['swipe_id'])
+        buyer = User.objects.get(user_id=data['user_id'])
+    except Swipe.DoesNotExist:
+        return Response({'STATUS': '1', 'REASON': 'SWIPE DOES NOT EXIST'}, status=status.HTTP_400_BAD_REQUEST)
+    except User.DoesNotExist:
+        return Response({'STATUS': '1', 'REASON': 'USER DOES NOT EXIST'}, status=status.HTTP_400_BAD_REQUEST)
+
+    if swipe.status == '0':
+        swipe.status = '1'
+        swipe_serializer = SwipeSerializer(swipe, data={'status': '1'}, partial=True)
+        if swipe_serializer.is_valid():
+            swipe_serializer.save()
+            client = Client(twilio_account_sid, twilio_auth_token)
+            message = client.messages.create(body=f"User {buyer.user_id} has bought your swipe!",
+                                             from_='+18563065093', to='+15106486565')
+        else:
+            return Response(swipe_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({'STATUS': '0', 'REASON': 'SWIPE SOLD'})
+    else:
+        return Response({'STATUS': '1', 'REASON': 'SWIPE ALREADY SOLD'})
