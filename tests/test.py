6af9c2ee251892749c73779cc5627567a2469ea4
Eric Dang <edang830@gmail.com>
2020-03-04 16:48:44 -0800

    Add test for default API

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,14 @@
 import requests
+import json
+
+
+def test_default():
+    r = requests.get('https://localhost:8000/api')
+    print(r.text)
 
 
 def main():
-    pass
+    test_default()
 
 
 if __name__ == "__main__":
