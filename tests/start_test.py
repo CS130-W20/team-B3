9114a36c29ef133a22412cb80b79fca0751974ef
cuixuesen <youippak47@gmail.com>
2020-03-10 00:15:51 -0700

    -TEST

--- tests/start_test.py
+++ tests/start_test.py
@@ -1,20 +1,24 @@
-import os, time, multiprocessing
+import os, time
+from multiprocessing import Process, Queue
+
+Q = Queue()
 
 def start_server():
     os.system("python3 backend/manage.py runserver")
 def start_testing():
     cmd = os.system("cd tests && python3 test.py")
-    assert(os.WEXITSTATUS(cmd) == 0)
+    Q.put(os.WEXITSTATUS(cmd))
 
 def main():
-    p1 = multiprocessing.Process(target=start_server)
-    p2 = multiprocessing.Process(target=start_testing)
+    p1 = Process(target=start_server)
+    p2 = Process(target=start_testing)
 
     p1.start()
     time.sleep(10)
     p2.start()
     p2.join()
     p1.terminate()
+    exit(Q.get())
 
 if __name__ == "__main__":
     main()
