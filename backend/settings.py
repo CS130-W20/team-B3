d91575e4fc0cf74cca1a69366d141b64ecb0116c
Taasin <taasin.saquib@gmail.com>
2020-03-11 11:09:18 -0700

    added allowed host

--- backend/settings.py
+++ backend/settings.py
@@ -7,6 +7,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+ALLOWED_HOSTS = ['swipex130.herokuapp.com']
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
