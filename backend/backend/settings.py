36aaed9228bcc0487b2f061082f629f707ad3c73
cuixuesen <youippak47@gmail.com>
2020-03-11 14:53:43 -0700

    Fixed small bugs

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e6m5-_qih_)wnfb%hlvnkjdkpvj7&(vb2+p=j=-&6%3lvjhd6w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['62796330.ngrok.io']
+ALLOWED_HOSTS = ['62796330.ngrok.io', 'localhost']
 
 
 # Application definition
