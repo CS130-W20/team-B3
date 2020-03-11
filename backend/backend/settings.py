4aa9667f771a456b76499c904f64e7bf03d23e98
Taasin <taasin.saquib@gmail.com>
2020-03-11 11:47:02 -0700

    generate SECRET_KEY

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,6 +1,8 @@
 import os
 import django_heroku
 
+from django.core.management.utils import get_random_secret_key
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -18,5 +20,7 @@ STATIC_URL = "/static/"
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
+
+SECRET_KEY = get_random_secret_key()
 
 django_heroku.settings(locals())
